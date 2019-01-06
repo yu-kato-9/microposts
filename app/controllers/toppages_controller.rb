f66578ebb46b3b39788a32b029cde7008fae53d8
yu.kato <yukato.gakushuyo@gmail.com>
2019-01-06 10:25:15 +0000

    post

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,4 +1,8 @@
 class ToppagesController < ApplicationController
   def index
+    if logged_in?
+      @micropost = current_user.microposts.build # form_for用
+      @microposts = current_user.microposts.order('created_at DESC').page(params[:page]) # 一覧用
+    end
   end
 end
