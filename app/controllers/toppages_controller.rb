e718a154b2b3ccb17c303af83b547c22cd234b47
yu.kato <yukato.gakushuyo@gmail.com>
2019-01-12 06:53:39 +0000

    time line

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -2,7 +2,7 @@ class ToppagesController < ApplicationController
   def index
     if logged_in?
       @micropost = current_user.microposts.build # form_for用
-      @microposts = current_user.microposts.order('created_at DESC').page(params[:page]) # 一覧用
+      @microposts = current_user.feed_microposts.order('created_at DESC').page(params[:page]) # 一覧用
     end
   end
 end
